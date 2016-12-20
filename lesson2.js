5ea0b8f3f5da946dff2e3796793ce03f73dd7f1d
Nick Kasten <kastencode@gmail.com>
2016-12-19 19:19:25 -0600

    updated Static Files lesson to work with actual local files

--- lesson2.js
+++ lesson2.js
@@ -1,6 +1,7 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
-app.use(express.static(process.argv[3]))
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(process.argv[2])
 
